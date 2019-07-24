cebad9d2b894ed1c96701d3426745fbd72827449
Manoj Kumar Desai <manoj.kumar.desai@in.verizon.com>
2019-07-24 07:55:53 +0530

    Firebase configuration - part 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA4EE1oIODSpEQLAYbjBHLtRXx8ahpFJ1U",
+    authDomain: "oshop-f58b0.firebaseapp.com",
+    databaseURL: "https://oshop-f58b0.firebaseio.com",
+    projectId: "oshop-f58b0",
+    storageBucket: "",
+    messagingSenderId: "741673014815",
+    appId: "1:741673014815:web:59eec30a2b9cdcb1"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA4EE1oIODSpEQLAYbjBHLtRXx8ahpFJ1U",
+    authDomain: "oshop-f58b0.firebaseapp.com",
+    databaseURL: "https://oshop-f58b0.firebaseio.com",
+    projectId: "oshop-f58b0",
+    storageBucket: "",
+    messagingSenderId: "741673014815",
+    appId: "1:741673014815:web:59eec30a2b9cdcb1"
+  }
 };
 
 /*
